ffe8927aed2ae27488b1dc3e66e9e8e93288bfe5
Lee <lsmin476@naver.com>
2023-07-01 19:46:13 +0900

    |ADD| 멀티쓰레드일 경우 싱글톤패턴 0701

--- DesignPattern/src/CreationalPattern/Singleton.java
+++ DesignPattern/src/CreationalPattern/Singleton.java
@@ -17,13 +17,49 @@ public class Singleton {
     }
 }
 
+
+// 멀티스레드에서의 싱글톤1
+class MultiThreadSingleton1{
+    // 초기에 static 변수로 singleton 인스턴스를 생성
+    private static MultiThreadSingleton1 instance = new MultiThreadSingleton1();
+
+    private MultiThreadSingleton1(){ }
+
+    public static MultiThreadSingleton1 getInstance(){
+        return instance;
+    }
+}
+
+// 멀티스레드에서의 싱글톤2
+class MultiThreadSingleton2{
+    private static MultiThreadSingleton2 instance;
+
+    private MultiThreadSingleton2(){ }
+
+    public static synchronized MultiThreadSingleton2 getInstance(){
+        // instance 가 없을 때만 생성
+        if (instance == null)
+            instance = new MultiThreadSingleton2();
+
+        return instance;
+    }
+}
+
 // 같은 instance 인지 Test
 class Test{
     public static void main(String[] args) {
         Singleton singleton1 = Singleton.getInstance();
         Singleton singleton2 = Singleton.getInstance();
+        MultiThreadSingleton1 multiThreadSingleton1 = MultiThreadSingleton1.getInstance();
+        MultiThreadSingleton1 multiThreadSingleton2 = MultiThreadSingleton1.getInstance();
+        MultiThreadSingleton2 multiThreadSingleton21 = MultiThreadSingleton2.getInstance();
+        MultiThreadSingleton2 multiThreadSingleton22 = MultiThreadSingleton2.getInstance();
 
         System.out.println("singleton1 = " + singleton1);
         System.out.println("singleton2 = " + singleton2);
+        System.out.println("multiThreadSingleton1 = " + multiThreadSingleton1);
+        System.out.println("multiThreadSingleton2 = " + multiThreadSingleton2);
+        System.out.println("multiThreadSingleton21 = " + multiThreadSingleton21);
+        System.out.println("multiThreadSingleton22 = " + multiThreadSingleton22);
     }
 }
