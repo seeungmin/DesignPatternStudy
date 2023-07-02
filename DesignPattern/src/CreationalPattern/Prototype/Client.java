2dbc723d26d96c0da77fc863b4eb30eb7b077f37
Lee <lsmin476@naver.com>
2023-07-03 01:05:36 +0900

    |UPDATE| 프로토타입 패턴 0703

--- DesignPattern/src/CreationalPattern/Prototype/Client.java
+++ DesignPattern/src/CreationalPattern/Prototype/Client.java
@@ -1,2 +1,23 @@
-package CreationalPattern.Prototype;public class Client {
+package CreationalPattern.Prototype;
+
+import java.util.List;
+
+public class Client {
+    public static void main(String[] args) throws CloneNotSupportedException{
+        Major major = new Major();
+        major.listAll();
+
+        Major major1 = (Major) major.clone();
+        Major major2 = (Major) major.clone();
+
+        List<String> list1 = major1.getList();
+        List<String> list2 = major2.getList();
+
+        list1.add("Algorithm");
+        list2.remove("Software");
+
+        System.out.println("list = " + major.getList());
+        System.out.println("list1 = " + list1);
+        System.out.println("list2 = " + list2);
+    }
 }
--- DesignPattern/src/CreationalPattern/Prototype/Major.java
+++ DesignPattern/src/CreationalPattern/Prototype/Major.java
@@ -1,2 +1,33 @@
-package CreationalPattern.Prototype;public class Major {
+package CreationalPattern.Prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Major implements Cloneable{
+
+    private List<String> list;
+
+    public Major() {
+        this.list = new ArrayList<>();
+    }
+
+    public Major(List<String> list) {
+        this.list = list;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void listAll() {
+        this.list.add("Mobile");
+        this.list.add("Network");
+        this.list.add("OperatingSystem");
+        this.list.add("Software");
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Major(new ArrayList<>(this.list));
+    }
 }
