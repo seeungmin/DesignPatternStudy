3ebacf47a327f681b0f38bfcc5bbc99c6e78b1cb
Lee <lsmin476@naver.com>
2023-07-04 18:40:33 +0900

    |UPDATE| 싱글톤 패턴 수정본2 0704

--- DesignPattern/src/CreationalPattern/Singleton/Test.java
+++ DesignPattern/src/CreationalPattern/Singleton/Test.java
@@ -9,6 +9,8 @@ public class Test{
         Singleton1 singleton12 = Singleton1.getInstance();
         Singleton2 singleton21 = Singleton2.getInstance();
         Singleton2 singleton22 = Singleton2.getInstance();
+        Singleton3 singleton31 = Singleton3.getInstance();
+        Singleton3 singleton32 = Singleton3.getInstance();
 
         System.out.println("singleton1 = " + singleton1);
         System.out.println("singleton2 = " + singleton2);
@@ -16,5 +18,7 @@ public class Test{
         System.out.println("singleton12 = " + singleton12);
         System.out.println("singleton21 = " + singleton21);
         System.out.println("singleton22 = " + singleton22);
+        System.out.println("singleton31 = " + singleton31);
+        System.out.println("singleton32 = " + singleton32);
     }
 }
